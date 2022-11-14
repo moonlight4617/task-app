0ec0a793493f388b51cbe08dee9815e90b6a86db
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2022-11-14 23:16:28 +0900

    設定画面 定常タスクを追加できるよう実装

--- src/components/SettingScreen.jsx
+++ src/components/SettingScreen.jsx
@@ -3,29 +3,67 @@ import "./SettingScreen.css";
 import { collection, addDoc, getDocs, query, where, updateDoc, deleteDoc, doc } from "firebase/firestore";
 import { db } from '../firebase';
 import { useEffect, useState } from "react";
+import Select from 'react-select'
 
 
 export function SettingScreen() {
   const [MHListFromDB, setMHListFromDB] = useState([]);
   const [regularTask, setRegularTask] = useState([]);
   const [inputText, setInputText] = useState("");
+  const [inputRegularTask, setInputRegularTask] = useState("");
   const [inputHour, setInputHour] = useState("");
+  const [inputRegularHour, setInputRegularHour] = useState("");
   const [selectedPerson, setSelectedPerson] = useState([]);
+  const [selectedSpecific, setSelectedSpecific] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
+  const [picFlag, setPicFlag] = useState(false);
+  const [specificFlag, setSpecificFlag] = useState(false);
+
+
+  const persons = MHListFromDB.map((mh) => (
+    { value: mh.pic, label: mh.pic }
+  ))
+
+  const specifics = [
+    { value: "毎日", label: "毎日" },
+    { value: "日曜", label: "日曜" },
+    { value: "月曜", label: "月曜" },
+    { value: "火曜", label: "火曜" },
+    { value: "水曜", label: "水曜" },
+    { value: "木曜", label: "木曜" },
+    { value: "金曜", label: "金曜" },
+    { value: "土曜", label: "土曜" },
+    { value: "月初", label: "月初" },
+    { value: "月末", label: "月末" },
+  ];
 
   const onChangeText = (e) => {
     setInputText(e.target.value);
+  };
+
+  const onChangeRegularTask = (e) => {
+    setInputRegularTask(e.target.value);
   };
 
   const onChangeHour = (e) => {
     setInputHour(e.target.value);
   };
 
-  // const onHandlePerson = (e) => {
-  //   const persons = e.map((person) => (person.value))
-  //   setSelectedPerson(persons);
-  //   setPicFlag(true);
-  // };
+  const onChangeRegularHour = (e) => {
+    setInputRegularHour(e.target.value);
+  };
+
+  const onHandlePerson = (e) => {
+    const persons = e.map((person) => (person.value))
+    setSelectedPerson(persons);
+    setPicFlag(true);
+  };
+
+  const onHandleSpecific = (e) => {
+    const specific = e.map((specific) => (specific.value))
+    setSelectedSpecific(specific);
+    setSpecificFlag(true);
+  };
 
   const onClickAdd = async () => {
     if (inputText === "") return;
@@ -39,10 +77,34 @@ export function SettingScreen() {
         // operatingTime: inputHour
         ...newPic
       });
-      console.log(newPic);
+      // console.log(newPic);
       setMHListFromDB([...MHListFromDB, newPic])
       setInputText("");
       setInputHour("");
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
+  const onClickAddRegularTask = async () => {
+    if (inputRegularTask === "") return;
+    // console.log(selectedSpecific);
+    try {
+      const docRef = await addDoc(collection(db, "daily"), {
+        taskName: inputRegularTask,
+        pic: selectedPerson,
+        // category: selectedCategory,
+        taskHour: inputRegularHour,
+        completeFlag: false,
+        regular: selectedSpecific
+      });
+      const newRegularTask = { id: docRef.id, taskName: inputRegularTask, pic: selectedPerson, taskHour: inputRegularHour, completeFlag: false, regular: selectedSpecific }
+      setRegularTask([...regularTask, newRegularTask])
+      setInputRegularTask("");
+      setInputRegularHour("");
+      setPicFlag(false);
+      setSpecificFlag(false);
       console.log("Document written with ID: ", docRef.id);
     } catch (e) {
       console.error("Error adding document: ", e);
@@ -55,7 +117,7 @@ export function SettingScreen() {
     const MHquerySnapshot = query(collection(db, "manHours"));
     const querySnapshot = query(collection(db, "daily"), where("regular", "!=", null));
 
-    // DBから本日タスク取得
+    // DBから定常タスク取得
     getDocs(querySnapshot)
       .then((snapShot) => {
         snapShot.forEach((doc) => {
@@ -75,7 +137,7 @@ export function SettingScreen() {
           // console.log(doc.data());
           MHList.push(doc.data());
         })
-        console.log(MHList);
+        // console.log(MHList);
         setMHListFromDB(MHList);
       });
   }, [])
@@ -85,6 +147,7 @@ export function SettingScreen() {
       <h3>設定</h3>
       <div className='setting'>
         <div className='setting-section'>
+          <h4>メンバー 一覧</h4>
           <table>
             <thead>
               <tr>
@@ -111,7 +174,8 @@ export function SettingScreen() {
             <button className="button" onClick={onClickAdd}>作成</button>
           </div>
         </div>
-        <div className='setting-section'>定常タスク
+        <div className='setting-section'>
+          <h4>定常タスク</h4>
           <table>
             <thead>
               <tr>
@@ -132,6 +196,17 @@ export function SettingScreen() {
               ))}
             </tbody>
           </table>
+          <input placeholder="タスク名"
+            value={inputRegularTask}
+            onChange={onChangeRegularTask}
+            className="">
+          </input>
+          <Select placeholder="担当者" options={persons} value={picFlag && persons.value} onChange={onHandlePerson} isMulti id="pic" className="pic-select" />
+          <input type="number" min="0" placeholder="工数" value={inputRegularHour} onChange={onChangeRegularHour} className="hm" /><br />
+          <Select placeholder="指定日" options={specifics} value={specificFlag && specifics.value} onChange={onHandleSpecific} isMulti id="pic" className="pic-select" />
+          <div className="button-area">
+            <button className="button" onClick={onClickAddRegularTask}>作成</button>
+          </div>
         </div>
       </div>
     </>
