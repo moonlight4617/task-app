e1775c6d61d518dabffe374aedec28969536e991
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2022-11-23 02:30:12 +0900

    react-modal導入。設定画面で編集、削除できるよう実装

--- src/components/SettingScreen.jsx
+++ src/components/SettingScreen.jsx
@@ -1,25 +1,38 @@
 import React from 'react'
 import "./SettingScreen.css";
-import { collection, addDoc, getDocs, query, where, updateDoc, deleteDoc, doc } from "firebase/firestore";
+import { collection, addDoc, getDocs, query, where, updateDoc, deleteDoc, doc, setDoc } from "firebase/firestore";
 import { db } from '../firebase';
 import { useEffect, useState } from "react";
 import Select from 'react-select'
 import { EditRegularModal } from './EditRegularModal.jsx'
+import Modal from 'react-modal'
 
 export function SettingScreen() {
   const [MHListFromDB, setMHListFromDB] = useState([]);
   const [regularTask, setRegularTask] = useState([]);
-  const [inputText, setInputText] = useState("");
+  const [inputMember, setInputMember] = useState("");
+  const [inputEditMember, setInputEditMember] = useState("");
+  const [inputEditTask, setInputEditTask] = useState("");
   const [inputRegularTask, setInputRegularTask] = useState("");
   const [inputHour, setInputHour] = useState("");
+  const [inputEditMHour, setInputEditMHour] = useState("");
   const [inputRegularHour, setInputRegularHour] = useState("");
+  const [inputEditRegularHour, setInputEditRegularHour] = useState("");
   const [selectedPerson, setSelectedPerson] = useState([]);
+  const [taskDefaultPerson, setTaskDefaultPerson] = useState([]);
   const [selectedSpecific, setSelectedSpecific] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedEditSpecific, setSelectedEditSpecific] = useState([]);
+  const [selectedDefaultSpecific, setSelectedDefaultSpecific] = useState([]);
+  // const [selectedCategory, setSelectedCategory] = useState("");
   const [picFlag, setPicFlag] = useState(false);
   const [specificFlag, setSpecificFlag] = useState(false);
-  const [showEditModal, setShowEditModal] = useState(false);
-  const [editTask, setEditTask] = useState("");
+  // const [showEditModal, setShowEditModal] = useState(false);
+  // const [editTask, setEditTask] = useState("");
+  // const [regularDay, setRegularDay] = useState("");
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [memberModalIsOpen, setMemberModalIsOpen] = useState(false);
+  const [modalTask, setModalTask] = useState("");
+
 
 
   const persons = MHListFromDB.map((mh) => (
@@ -39,21 +52,111 @@ export function SettingScreen() {
     { value: "月末", label: "月末" },
   ];
 
-  const onChangeText = (e) => {
-    setInputText(e.target.value);
+  // モーダル用css
+  var subtitle;
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      // right: 'auto',
+      // bottom: 'auto',
+      // marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      width: '50%'
+    }
   };
 
-  const onChangeRegularTask = (e) => {
+  function openModal(editTask) {
+    // タスクの担当者が選択済みであれば、表示されるように
+    let personIndex = [];
+    if (editTask.pic.length > 0) {
+      editTask.pic.forEach((person) => {
+        var index = persons.findIndex(e => e.value == person);
+        if (index !== -1) {
+          personIndex.push(index);
+        }
+      })
+    }
+    const picList = personIndex.map((p) => persons[p])
+    setTaskDefaultPerson(picList);
+    const picListValue = picList.map((pic) => (pic.value))
+    setSelectedPerson(picListValue);
+
+    // 指定日があれば表示されるように
+    let regularIndex = [];
+    if (editTask.regular.length > 0) {
+      editTask.regular.forEach((regular) => {
+        var index = specifics.findIndex(e => e.value == regular);
+        if (index !== -1) {
+          regularIndex.push(index);
+        }
+      })
+    }
+    const regularList = regularIndex.map((p) => specifics[p])
+    setSelectedDefaultSpecific(regularList);
+    const regularListValue = regularList.map((regular) => (regular.value))
+    setSelectedEditSpecific(regularListValue);
+
+    setModalTask(editTask);
+    setInputEditTask(editTask.taskName);
+    setInputEditRegularHour(editTask.taskHour);
+    setIsOpen(editTask.id);
+  }
+
+  function openMemberModal(member) {
+    setInputEditMember(member.pic);
+    setInputEditMHour(member.operatingTime);
+    setMemberModalIsOpen(member.id);
+  }
+
+
+  function afterOpenModal() {
+    subtitle.style.color = '#3ab60b';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function closeMemberModal() {
+    setMemberModalIsOpen(false);
+  }
+
+  const onChangeMemberName = (e) => {
+    setInputMember(e.target.value);
+  };
+
+  const onChangeEditMemberName = (e) => {
+    setInputEditMember(e.target.value);
+  };
+
+  const onChangeTaskName = (e) => {
     setInputRegularTask(e.target.value);
+  };
+
+  const onChangeEditTaskName = (e) => {
+    setInputEditTask(e.target.value);
   };
 
   const onChangeHour = (e) => {
     setInputHour(e.target.value);
   };
 
+  const onChangeEditMHour = (e) => {
+    setInputEditMHour(e.target.value);
+  };
+
   const onChangeRegularHour = (e) => {
     setInputRegularHour(e.target.value);
   };
+
+  const onChangeEditRegularHour = (e) => {
+    setInputEditRegularHour(e.target.value);
+  };
+
+  // const onChangeRegularDay = (e) => {
+  //   setRegularDay(e.target.value);
+  // };
 
   const onHandlePerson = (e) => {
     const persons = e.map((person) => (person.value))
@@ -67,32 +170,39 @@ export function SettingScreen() {
     setSpecificFlag(true);
   };
 
-  const onClickEditTask = (e) => {
-    const edit = regularTask.find(task => task.id == e.target.id);
-    setEditTask(edit);
-    // console.log(edit);
-    setShowEditModal(true);
-  }
+  const onHandleEditSpecific = (e) => {
+    const specific = e.map((specific) => (specific.value))
+    console.log(specific);
+    setSelectedEditSpecific(specific);
+    // setSpecificFlag(true);
+  };
 
-  const onClickCloseEditModal = () => {
-    setShowEditModal(false);
-  }
+  // const onClickEditTask = (e) => {
+  //   const edit = regularTask.find(task => task.id == e.target.id);
+  //   setEditTask(edit);
+  //   setShowEditModal(true);
+  // }
 
-  const onClickAdd = async () => {
-    if (inputText === "") return;
-    const newPic = {
-      pic: inputText,
-      operatingTime: inputHour
-    };
+  // const onClickCloseEditModal = () => {
+  //   setShowEditModal(false);
+  // }
+
+  const onClickAddMember = async () => {
+    if (inputMember === "") return;
     try {
       const docRef = await addDoc(collection(db, "manHours"), {
-        // pic: inputText,
-        // operatingTime: inputHour
-        ...newPic
+        pic: inputMember,
+        operatingTime: inputHour
+        // ...newPic
       });
       // console.log(newPic);
+      const newPic = {
+        id: docRef.id,
+        pic: inputMember,
+        operatingTime: inputHour
+      };
       setMHListFromDB([...MHListFromDB, newPic])
-      setInputText("");
+      setInputMember("");
       setInputHour("");
       console.log("Document written with ID: ", docRef.id);
     } catch (e) {
@@ -102,12 +212,10 @@ export function SettingScreen() {
 
   const onClickAddRegularTask = async () => {
     if (inputRegularTask === "") return;
-    // console.log(selectedSpecific);
     try {
       const docRef = await addDoc(collection(db, "daily"), {
         taskName: inputRegularTask,
         pic: selectedPerson,
-        // category: selectedCategory,
         taskHour: inputRegularHour,
         completeFlag: false,
         regular: selectedSpecific
@@ -122,6 +230,84 @@ export function SettingScreen() {
     } catch (e) {
       console.error("Error adding document: ", e);
     }
+  };
+
+  const onClickUpdateRegularTask = async (task) => {
+    if (inputEditTask === "") return;
+    const editTask = {
+      id: task.id,
+      taskName: inputEditTask,
+      pic: selectedPerson,
+      regular: selectedEditSpecific,
+      taskHour: inputEditRegularHour
+    };
+
+    const newRegularTaskList = regularTask.map((t) => {
+      if (t.id !== task.id) {
+        return t
+      } else {
+        return editTask
+      }
+    })
+    setRegularTask(newRegularTaskList);
+
+    try {
+      await setDoc(doc(db, "daily", task.id), {
+        taskName: inputEditTask,
+        pic: selectedPerson,
+        regular: selectedEditSpecific,
+        taskHour: inputEditRegularHour
+      });
+      console.log("Document written with ID: ", task.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    } finally {
+      closeModal();
+    }
+  };
+
+  const onClickUpdateMember = async (member) => {
+    if (inputEditMember === "") return;
+    const editMember = {
+      id: member.id,
+      pic: inputEditMember,
+      operatingTime: inputEditMHour
+    };
+    const newMHList = MHListFromDB.map((list) => {
+      if (list.id !== member.id) {
+        return list
+      } else {
+        return editMember
+      }
+    })
+    setMHListFromDB(newMHList);
+
+    try {
+      await setDoc(doc(db, "manHours", member.id), {
+        pic: inputEditMember,
+        operatingTime: inputEditMHour
+      });
+      console.log("Document written with ID: ", member.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    } finally {
+      closeMemberModal();
+    }
+  };
+
+  const onClickDeleteMember = async (id, index) => {
+    const NewMHList = [...MHListFromDB];
+    NewMHList.splice(index, 1);
+    await deleteDoc(doc(db, "manHours", id));
+    setMHListFromDB(NewMHList);
+  };
+
+  const onClickDeleteRegularTask = async (id, index) => {
+    const newRegularTask = [...regularTask];
+    newRegularTask.splice(index, 1);
+    await deleteDoc(doc(db, "daily", id));
+    setRegularTask(newRegularTask);
+    // setTaskHour(MHListFromDB, newIncompleteList)
   };
 
   useEffect(() => {
@@ -148,7 +334,9 @@ export function SettingScreen() {
       .then((snapShot) => {
         snapShot.forEach((doc) => {
           // console.log(doc.data());
-          MHList.push(doc.data());
+          const id = { "id": doc.id }
+          const menberList = { ...id, ...doc.data() };
+          MHList.push(menberList);
         })
         // console.log(MHList);
         setMHListFromDB(MHList);
@@ -170,21 +358,51 @@ export function SettingScreen() {
             </thead>
             <tbody>
               {MHListFromDB.map((mh, index) => (
-                <tr key={index}>
-                  <td>{mh.pic}</td>
-                  <td>{mh.operatingTime}h</td>
-                </tr>
+                <div key={index}>
+                  <tr>
+                    <td onClick={() => openMemberModal(mh)}>{mh.pic}</td>
+                    <td>{mh.operatingTime}h</td>
+                    <button className="icon" onClick={() => onClickDeleteMember(mh.id, index)}>
+                      <i className="fa-solid fa-trash"></i>
+                    </button>
+                  </tr>
+                  <Modal
+                    isOpen={mh.id == memberModalIsOpen}
+                    onAfterOpen={afterOpenModal}
+                    onRequestClose={closeMemberModal}
+                    contentLabel="Example Modal"
+                    ariaHideApp={false}
+                    overlayClassName="overlay"
+                    style={customStyles}
+                  >
+                    <h2 ref={_subtitle => (subtitle = _subtitle)}>メンバー編集</h2>
+                    <div className="input-area">
+                      <input placeholder="メンバー名"
+                        value={inputEditMember}
+                        onChange={onChangeEditMemberName}
+                        className="">
+                      </input>
+                      <input type="number" min="0" placeholder="稼働可能時間" value={inputEditMHour} onChange={onChangeEditMHour} className="hm" />
+                      <div className="button-area">
+                        <button className="button" onClick={() => onClickUpdateMember(mh)}>更新</button>
+                      </div>
+                    </div>
+                    <button className="" onClick={closeMemberModal}>close</button>
+                  </Modal>
+                </div>
               ))}
             </tbody>
           </table>
-          <input placeholder="メンバー名"
-            value={inputText}
-            onChange={onChangeText}
-            className="">
-          </input>
-          <input type="number" min="0" placeholder="稼働可能時間" value={inputHour} onChange={onChangeHour} className="hm" />
+          <div className="select-area">
+            <input placeholder="メンバー名"
+              value={inputMember}
+              onChange={onChangeMemberName}
+              className="">
+            </input>
+            <input type="number" min="0" placeholder="稼働可能時間" value={inputHour} onChange={onChangeHour} className="hm" />
+          </div>
           <div className="button-area">
-            <button className="button" onClick={onClickAdd}>作成</button>
+            <button className="button" onClick={onClickAddMember}>作成</button>
           </div>
         </div>
         <div className='setting-section'>
@@ -202,23 +420,73 @@ export function SettingScreen() {
             <tbody>
               {regularTask.map((task, index) => (
                 <tr key={index}>
-                  <td onClick={onClickEditTask} id={task.id}>{task.taskName}</td>
+                  {/* <td onClick={onClickEditTask} id={task.id}>{task.taskName}</td> */}
+                  <td onClick={() => openModal(task)}>{task.taskName}</td>
                   <td>{task.pic}</td>
                   <td>{task.taskHour}</td>
                   <td>{task.regular}</td>
-                  <EditRegularModal task={editTask} showEditModal={showEditModal} onClickCloseEditModal={onClickCloseEditModal} persons={persons} />
+                  <Modal
+                    isOpen={task.id == modalIsOpen}
+                    onAfterOpen={afterOpenModal}
+                    onRequestClose={closeModal}
+                    contentLabel="Example Modal"
+                    ariaHideApp={false}
+                    overlayClassName="overlay"
+                    style={customStyles}
+                  >
+                    <h2 ref={_subtitle => (subtitle = _subtitle)}>{modalTask.taskName}</h2>
+                    <div className="input-area">
+                      <input
+                        placeholder="タスクを入力"
+                        value={inputEditTask}
+                        onChange={onChangeEditTaskName}
+                        className="task-text"
+                      />
+                      <div className="select-area">
+                        <Select
+                          placeholder="担当者"
+                          options={persons}
+                          // value={persons.value}
+                          defaultValue={taskDefaultPerson}
+                          onChange={onHandlePerson}
+                          isMulti id="pic"
+                          className="task-pic" />
+                        <input type="number" min="0" placeholder="工数" value={inputEditRegularHour} onChange={onChangeEditRegularHour} className="hm" /><br />
+                        <Select
+                          placeholder="指定日"
+                          options={specifics}
+                          defaultValue={selectedDefaultSpecific}
+                          onChange={onHandleEditSpecific}
+                          isMulti id="pic"
+                          className="task-pic" />
+                      </div>
+                      <div className="button-area">
+                        <button className="button" onClick={() => onClickUpdateRegularTask(modalTask)}>更新</button>
+                      </div>
+                    </div>
+                    <button className="" onClick={closeModal}>close</button>
+                  </Modal>
+
+
+
+                  <button className="icon" onClick={() => onClickDeleteRegularTask(task.id, index)}>
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
                 </tr>
               ))}
             </tbody>
           </table>
+
           <input placeholder="タスク名"
             value={inputRegularTask}
-            onChange={onChangeRegularTask}
+            onChange={onChangeTaskName}
             className="">
           </input>
-          <Select placeholder="担当者" options={persons} value={picFlag && persons.value} onChange={onHandlePerson} isMulti id="pic" className="pic-select" />
-          <input type="number" min="0" placeholder="工数" value={inputRegularHour} onChange={onChangeRegularHour} className="hm" /><br />
-          <Select placeholder="指定日" options={specifics} value={specificFlag && specifics.value} onChange={onHandleSpecific} isMulti id="pic" className="pic-select" />
+          <div className="select-area">
+            <Select placeholder="担当者" options={persons} value={picFlag && persons.value} onChange={onHandlePerson} isMulti id="pic" className="pic-select" />
+            <input type="number" min="0" placeholder="工数" value={inputRegularHour} onChange={onChangeRegularHour} className="hm" /><br />
+            <Select placeholder="指定日" options={specifics} value={specificFlag && specifics.value} onChange={onHandleSpecific} isMulti id="pic" className="pic-select" />
+          </div>
           <div className="button-area">
             <button className="button" onClick={onClickAddRegularTask}>作成</button>
           </div>
